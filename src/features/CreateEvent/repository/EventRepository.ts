7608e3f870881523f5b8315047020e0e8ca0b9bb
Tiep Minh Nguyen <tiep2403nbk@gmail.com>
2026-04-13 21:17:02 -0400

    feat: Initialize interface for Event Repository

---
+++ src/features/CreateEvent/repository/EventRepository.ts
@@ -0,0 +1,18 @@
+import { Result } from "../../../lib/result";
+import { IEvent } from "../model/event";
+import { EventError } from "./error";
+
+export interface CreateEventInput {
+  title: string;
+  description: string;
+  location: string;
+  category?: string;
+  capacity?: number;
+  startDatetime: Date;
+  endDatetime: Date;
+  organizerId: string;
+}
+
+export interface IEventRepository {
+  add(data: CreateEventInput): Promise<Result<IEvent, EventError>>;
+}
