cead9635661c5ee82c7266b3e8f56d475e088fa4
MMahaseth <mmahaseth@umass.edu>
2026-04-15 16:07:52 -0400

    feat: finalize Murali feature folders

---
+++ src/features/PastEventArchiving/PastEventArchivingController.ts
@@ -0,0 +1,45 @@
+import type { Request, Response } from "express";
+import type { IPastEventArchivingService } from "./PastEventArchivingService";
+import type { AppSessionStore } from "../../session/AppSession";
+import { getAuthenticatedUser, touchAppSession } from "../../session/AppSession";
+
+export interface IPastEventArchivingController {
+  showArchive(req: Request, res: Response): Promise<void>;
+}
+
+class PastEventArchivingController implements IPastEventArchivingController {
+  constructor(private readonly service: IPastEventArchivingService) {}
+
+  async showArchive(req: Request, res: Response): Promise<void> {
+    const store = req.session as AppSessionStore;
+    const currentUser = getAuthenticatedUser(store);
+    const session = touchAppSession(store);
+
+    if (!currentUser) {
+      res.redirect("/login");
+      return;
+    }
+
+    const result = await this.service.getArchivedEvents();
+
+    if (!result.ok) {
+      res.status(500).render("partials/error", {
+        message: result.value.message,
+        layout: false,
+      });
+      return;
+    }
+
+    res.render("events/archive", {
+      session,
+      events: result.value,
+      pageError: null,
+    });
+  }
+}
+
+export function CreatePastEventArchivingController(
+  service: IPastEventArchivingService,
+): IPastEventArchivingController {
+  return new PastEventArchivingController(service);
+}
