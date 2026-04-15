36ef4431a86b0744313e4ae72e72b97da6ee860a
Tiep Minh Nguyen <tiep2403nbk@gmail.com>
2026-04-15 15:56:42 -0400

    fix: fix typo in the add() method

--- src/features/CreateEvent/repository/InMemoryEventRepository.ts
+++ src/features/CreateEvent/repository/InMemoryEventRepository.ts
@@ -1,4 +1,3 @@
-import { title } from "node:process";
 import { Err, Ok, Result } from "../../../lib/result";
 import { createEvent, IEvent } from "../model/event";
 import { EventError, ValidationError } from "./error";
@@ -10,7 +9,7 @@ class InMemoryEventRepository implements IEventRepository {
   async add(data: CreateEventInput): Promise<Result<IEvent, EventError>> {
     const id = crypto.randomUUID();
     if (
-      !title ||
+      !data.title ||
       !data.description ||
       !data.location ||
       !data.startDatetime ||
